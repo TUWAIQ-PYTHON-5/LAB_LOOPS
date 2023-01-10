d919912fe7946155d135949e0e8ad531eb1c7fe9
sultan <sultan_33_886@outlook.sa>
2023-01-10 18:39:12 +0300

    the project lap loops

--- lab loop.py
+++ lab loop.py
@@ -13,8 +13,9 @@ for varblies in range(45,210):
 
 
 #qusiton2
-user = int(input("what is the product of 7 * 24 ?")) 
-while user == 168: 
+user = int(input("what is the product of 7 * 24 ?"))
+while user == 168:
+    user +=1
     print("You answered this Question correctly")
     break
 else:
