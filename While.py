b015de084e500196a3df07b2a45f996d38e79e6d
NjoudAlhazmi <njoudz19@gmail.com>
2023-01-10 19:42:03 +0300

    loop in python1

--- While.py
+++ While.py
@@ -1,19 +1,17 @@
-#The range function
-for number in range(45,210):
 
+
+for number in range(45, 210):
     if number == 100:
         continue
-    elif number == 205:
+    if number == 205:
         break
-        print(number)
+    print (number)
 
 
 
-#to get input from the user
-product: str = input("what is the product of 7 * 24 ?")
-
-while product == 168 :
-    print("You answered this Question correctly")
-else :
-    print("Your Answer is wrong try again..")
-
+user_input: str = input("what is the product of 7 * 24 ?" )
+while  user_input == "168":
+     print("You answered this Question correctly")
+     break
+else:    
+    print("Your Answer is wrong try again.. ")
