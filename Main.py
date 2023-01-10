fa62b51339e033ea4e195536ecd467e898c169fa
Ahmed Alhisan <alhisan.swe@gmail.com>
2023-01-10 14:51:27 +0300

    loops Lab solv

---
+++ Main.py
@@ -0,0 +1,30 @@
+
+
+for number in range(45 , 210):
+     if number == 100:
+         continue
+     elif number == 205:
+         break
+     print(number)
+else:
+     print("Done")
+
+     
+
+
+result : int =int(input("7 * 24"))
+
+while(result != 168):
+    print("Your Answer is wrong try again..")
+    result : int =int(input("7 * 24")) 
+    
+else:
+    print("You answered this Question correctly")    
+    
+    
+
+
+
+
+    
+
