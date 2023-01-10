f061e81ecd048a4c758a6a2b3de68f0b97c72fee
atheer <atheeralq0@gmail.com>
2023-01-10 21:06:17 +0300

    lab solved

---
+++ lab2.py
@@ -0,0 +1,20 @@
+
+for Numbers in range(45,210):
+
+    if Numbers == 100:
+        continue
+        print(Numbers)
+    elif Numbers == 205:
+        break
+
+
+x=True
+while x:
+ product : int = input("what is the product of 7 * 24 ? ")
+ if product == 168: 
+    x=False
+ else: 
+    print("Your Answer is wrong try again.") 
+    continue
+else :
+    print("You answered this Question correctly")
