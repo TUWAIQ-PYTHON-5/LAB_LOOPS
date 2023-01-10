14b53950c93db72edee13e3dc64d2c8411dfa0b2
Faimubarak <102253601+Faimubarak@users.noreply.github.com>
2023-01-11 01:13:54 +0300

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,25 @@
+
+#range 
+
+for number in range(45,210):
+     if (number == 100): 
+         continue
+     print(number)
+     if (number == 205):
+          break 
+
+
+#while  
+user_n = int(input("what is the product of 7 * 24 ?"))
+i=1
+mul: int = 0
+while (i<=user_n):
+ mul = mul * i
+i = i + 1
+
+print( "You answered this Question correctly")             
+    elif
+    print("Your Answer is wrong try again..")
+
+else:
+print("try again")
