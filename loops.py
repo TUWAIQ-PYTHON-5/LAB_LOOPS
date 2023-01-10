40d8ab0ee59f478a81a3db5201a2f773aeb64417
halrasheed <haroon.arrsheed@gmail.com>
2023-01-10 16:06:01 +0300

    Loops Lab upload

--- loops.py
+++ loops.py
@@ -1,14 +1,17 @@
-for number in range(45, 210):
+#1 
+for number in range(45, 210, 1):
     if number == 100:
         continue
     elif number == 205:
         break
+    print(number)
 
+#2
+'''
+'''
 
-
-    usrAnswer: int = int(input("What is the prodect of 7 x 24 ?"))
-    while usrAnswer != 168: 
+usrAnswer: int = int(input("What is the prodect of 7 x 24 ?"))
+while usrAnswer != 168: 
         usrAnswer: int = int(input("Your Answer is wrong, try again"))
-    else:
-        print("You answerd this Question correctly")
-    break
+else:
+    print("You answerd this Question correctly")
