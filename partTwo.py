184c5a3d1013f5714ffcf4e6be468ce9696e44da
Anfal Ali <name@gmail.com>
2023-01-11 10:59:10 +0300

    LAP_LOOPS

--- partTwo.py
+++ partTwo.py
@@ -1,12 +1,13 @@
 result : int =int(input("what is the product of 7 * 24 ? \n "))
 
-while result == 168:
-   print("You answered this Question correctly")
-   break
-else:
-   
+while int(result) != 168:
    print("Your Answer is wrong try again..")
    result : int = int(input("what is the product of 7 * 24 ? \n "))
+else:
+   print("You answered this Question correctly")
+
+   
+   
 
         
     
