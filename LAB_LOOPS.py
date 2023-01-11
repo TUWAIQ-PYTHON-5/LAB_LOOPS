716a6fda848750bfb9c3385db5f9460c4524c6a8
unknown <dalmq4806@gmail.comgit config --global user .name dalalgit config --global user.email dalmq4806@gmail.com>
2023-01-11 07:36:10 +0300

    lab loops

---
+++ LAB_LOOPS.py
@@ -0,0 +1,24 @@
+for numbers in range (45,210):
+
+    if numbers == 100:
+        continue
+
+    elif numbers == 205:
+        break
+    print(numbers)
+
+user_1 :str = input("what is the product of 7 * 24 ?")
+while user_1 == '168':
+    print("You answered this Question correctly")
+                 break
+    else:
+     print("Your Answer is wrong try again.")
+     user_1 :str = input("what is the product of 7 * 24 ?")
+     print("You answered this Question correctly")
+
+
+
+
+
+
+
