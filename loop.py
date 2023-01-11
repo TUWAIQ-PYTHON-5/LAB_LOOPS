665262c2a37bcd2b7729779033221da568e9a3fe
Faimubarak <102253601+Faimubarak@users.noreply.github.com>
2023-01-11 10:17:51 +0300

    Add files via upload

--- loop.py
+++ loop.py
@@ -9,17 +9,14 @@ for number in range(45,210):
           break 
 
 
-#while  
-user_n = int(input("what is the product of 7 * 24 ?"))
-i=1
-mul: int = 0
-while (i<=user_n):
- mul = mul * i
-i = i + 1
+#while 
 
-print( "You answered this Question correctly")             
-    elif
-    print("Your Answer is wrong try again..")
 
-else:
-print("try again")
+while True:
+    user_n = int(input("what is the product of 7 * 24 ?"))
+    if user_n == 168:
+       print( "You answered this Question correctly")  
+       break
+    else:
+      print("Your Answer is wrong try again..")
+
