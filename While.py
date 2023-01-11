e5ffd287a5e94f3d779590e1207660460df67203
NjoudAlhazmi <njoudz19@gmail.com>
2023-01-11 12:53:57 +0300

    the product

--- While.py
+++ While.py
@@ -1,5 +1,3 @@
-
-
 for number in range(45, 210):
     if number == 100:
         continue
@@ -9,9 +7,12 @@ for number in range(45, 210):
 
 
 
-user_input: str = input("what is the product of 7 * 24 ?" )
-while  user_input == "168":
-     print("You answered this Question correctly")
-     break
-else:    
-    print("Your Answer is wrong try again.. ")
+
+user_intpput = input("what is the product of 7 * 24 ?" )
+
+while int(user_intpput) != 7*24:
+      print("Your Answer is wrong try again..")
+      user_intpput = input ("what is the product of 7 * 24 ?")
+
+else :
+      print("You answered this Question correctly " ) 
